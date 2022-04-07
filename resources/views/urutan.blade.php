d3b64ce837c7d531b5859c1d3b27e39d691fbf7a
SnurAulia <snuraulia48@gmail.com>
2022-04-07 22:10:24 +0700

    Commit1

---
+++ resources/views/urutan.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('title', 'Urutan')
+
+@section('content')
+@foreach($numbers as $numbers)
+<h1>Urutan ke - {{$numbers['ke']}}</h1>
+<h3>Nomor ke - {{$numbers['nomor']}}</h3>
+@endforeach
+@endsection
